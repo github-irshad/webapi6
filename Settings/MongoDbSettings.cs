d78d82eedcd5db667b9677a7c2c029954d3954f3
irshad_aj <irshadaj08@gmail.com>
2022-10-05 15:11:05 +0530

    Docker installed , Changes in appsetting.json

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+namespace webapi6.Settings{
+  public class MongoDbSettings{
+    public string Host {get; set;}
+    public int Port{get; set;}
+
+    public string ConnectionString { get {
+      return $"mongodb://{Host}:{Port}";
+    }}
+  }
+}
