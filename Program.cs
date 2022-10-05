ecd6fbbfd891a592f2aaf882633f811d7cf501bd
irshad_aj <irshadaj08@gmail.com>
2022-10-05 10:27:58 +0530

    aaa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<IItemRepository,ItemRepository>();
+// builder.Services.AddSingleton<IItemRepository,ItemRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
